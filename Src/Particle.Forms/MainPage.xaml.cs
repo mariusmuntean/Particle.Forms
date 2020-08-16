300183bb303b6d151013eb67378e6043e29c73a0
Marius Muntean <marius.muntean@live.com>
2020-08-16 09:13:51 +0200

    Project setup

---
+++ Src/Particle.Forms/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Particle.Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
